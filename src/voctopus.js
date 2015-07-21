83fded7bca603d94aaa543c23e162514f090291f
Justen Robertson <justen@justen.us>
2015-07-21 15:51:52 -0700

    Finished basic octree implementation

--- src/voctopus.js
+++ src/voctopus.js
@@ -3,42 +3,287 @@
 
 /**
  * Returns length of side at depth d with maxdepth m:
- */
+ *
 function resOf(m, d) {
 	return 2*Math.pow(m-d);
+}*/
+
+/**
+ * We do lots of undefined checks, let's make it easier to type
+ */
+function isUndef(val) {
+ return typeof(val) === "undefined";
 }
+
+/**
+ * Functions below are defined here and bound in the Voctopus
+ * constructor. This way we get a single function definition
+ * while granting access to private properties of Voctopus.
+ * Quasi-namespaced to prevent conflicts in scripts that don't
+ * use commonjs' require() to load voctopus. Documentation is
+ * in the Voctopus object.
+ */
 
 /**
  * Index offset for depth d is sum of powers of 8 up to 8^d
  */
-function baseOffset(d) {
+function voctopusBaseOffset(d) {
 	return ~~((Math.pow(8, d) -1) / 7);
 }
 
+/**
+ * Checks a vector is valid
+ */
+function checkVec(v) {
+	if(isUndef(v) || isUndef(v.length) || v.length !== 3) throw new TypeError(v + " is not a vector");
+}
+
+/**
+ * @see Voctopus#octId
+ * Curried to Voctopus.octId(v, d).
+ */
+function voctopusOctId(_md, v, d) {
+	return (((v[2] >>> _md-d) & 1) << 2 | 
+					((v[1] >>> _md-d) & 1) << 1 |
+					((v[0] >>> _md-d) & 1) 
+				 );
+}
+
+/**
+ * @see Voctopus#octKey
+ * Curried to Voctopus.octKey(v)
+ */
+function voctopusOctKey(_md, v) {
+	var bo, o, d;
+	/* jshint validthis:true */
+	if(arguments.length < 3) d = _md;
+	else if(arguments[2] === 0) return 0; // anything at d=0 is always zero
+	else d = arguments[2];
+	bo = voctopusBaseOffset(d);
+	o = bo+(Math.pow(8, d-1)*this.octId(v, d-1))+this.octId(v, d);
+	return o;
+}
+
+/**
+ * Find the depth of a given key in a tree of depth _md
+ * Probably a faster way to do this
+ */
+function voctopusDepthOf(_md, key) {
+	for(var i = 0; i <= _md; i++) if(key >= voctopusBaseOffset(i) && key < voctopusBaseOffset(i+1)) return i;
+}
+
+/**
+ * Finds the nearest parent octet key for a child key
+ */
+function voctopusParentOfKey(_md, key) {
+	var d = voctopusDepthOf(_md, key);
+	return ~~((key-voctopusBaseOffset(d))/8)+voctopusBaseOffset(d-1);
+}
+
+/**
+ * Turns an octet identity into a vector of range [0,0,0]-[1,1,1].
+ * Inverse of voctopusIdOf().
+ */
+function voctopusVecOf(id) {
+	var vec = [0,0,0];
+	vec[0] = id & 1;
+	vec[1] = id >>> 1 & 1;
+	vec[2] = id >>> 2 & 1;
+	return vec;
+}
+
+/**
+ * Used internally to update a tree after updating a voxel. A better way to do this
+ * would be to pass parent octets directly, then just sweep their children. Will
+ * reimplement later.
+ * Curried to updateTree() in the Voctopus constructor.
+ */
+function voctopusUpdateTree(_elements, _updateQueue, _md) {
+	var i, n, d, queueKey, curKey, parentKey, octet, modeMap, modeMax, maxVal, modeLabel;
+	while(_updateQueue.length) {
+		queueKey = _updateQueue.pop(); // vec coord of voxel to update
+		if(isUndef(queueKey)) continue; // sometimes we just undef them because it's cheaper than splicing
+		// walk up the tree from coord, updating parents
+		for(d = _md; d > 0; d--) {
+			/* jshint validthis:true */
+			// find the curKey of the parent octet
+			parentKey = this.parentOfKey(queueKey);
+			// each octet is contiguous so we can use a shortcut to sweep it
+			// we just need the curKey of the 0th element of the octet, then sweep curKey+(0-7)
+			curKey = queueKey-((queueKey-voctopusBaseOffset(d))%8);
+			// grab the octet
+			octet = _elements.slice(curKey, curKey+n);
+			modeMap = {};
+			modeMax = 0;
+			maxVal = undefined;
+			// figure out the mathematical mode
+			for(n = 0; n < 8; n++) {
+				modeLabel = isUndef(_elements[curKey+n])?"undefined":_elements[curKey+n].toString();
+				modeMap[modeLabel] = isUndef(modeMap[modeLabel])?1:modeMap[modeLabel]+1;
+				if(modeMap[modeLabel] > modeMax) {
+					modeMax = modeMap[modeLabel];
+					maxVal = _elements[curKey+n];
+				}
+			}
+			// if the parent value didn't change skip update, otherwise set new val
+			// and mark it for updating
+			if(d > 0 && _elements[parentKey] !== maxVal) {
+				_elements[parentKey] = maxVal;
+				// only do this once per octet, no need to check other children
+				if(parentKey > 0 && _updateQueue.indexOf(parentKey) === -1) _updateQueue.push(parentKey);
+			}
+			// now prune the children if the octet is uniform
+			if(modeMax == 8) {
+				for(n = 0; n < 8; n++) {
+					_elements[curKey+n] = undefined;
+					// dequeue any children we pruned since they can't be processed
+					queueKey = _updateQueue.indexOf(curKey+n);
+					if(queueKey !== -1) _updateQueue[queueKey] = undefined; 
+				}
+			}
+		} // end depth loop
+	} // end queue loop 	
+}
+
+/**
+ * @see Voctopus#getVoxel
+ * Curried to Voctopus.getVoxel(v)
+ */
+function voctopusGetVoxel(_elements, v) {
+	/* jshint validthis:true */
+	var d = 0;
+	if(arguments.length === 3) d = arguments[3];
+	var val = _elements[this.octKey(v, d)];
+	if(isUndef(this.getVoxel(v, d+1))) return val; 
+	return val;
+}
+
+/**
+ * @see Voctopus#setVoxel
+ * @param _updateQueue {array} the current prune queue
+ * @param _elements {array} the octree's element array
+ * @param _md {int} the max depth of the octree
+ * @param _prune {bool} whether to execute a prune op
+ * @param v {vector} coordinate vector for the voxel to set
+ * @param val {mixed} value to give the vector
+ *
+ * Curried to Voctopus.setVoxel(v, val). Also used directly by
+ * voctopusSetVoxel.
+ */
+function voctopusSetVoxel(_updateQueue, _elements, _md, _prune, v, val) {
+	/* jshint validthis:true */
+	var id = this.octKey(v, _md);
+	_elements[id] = val;
+	_updateQueue.push(id);
+  if(_prune) this.updateTree();
+	return _elements[id];
+}
+
+/**
+ * @see Voctopus#setVoxels
+ * @param _updateQueue {array} the current prune queue
+ * @param _elements {array} the octree's element array
+ * @param _md {int} the max depth of the octree
+ *
+ * Sets multiple voxels at the same time, deferring the prune operation till the
+ * updates are complete. Curried to Voctopus.setVoxels(voxelData)
+ */
+function voctopusSetVoxels(_updateQueue, _elements, _md, voxelData) {
+	var i, key;
+	/* jshint validthis:true */
+	for(i = 0; i < voxelData.length; i++) {
+		voctopusSetVoxel.apply(this, [_updateQueue, _elements, _md, false, voxelData[i][0], voxelData[i][1]]);
+	}
+	this.updateTree();
+}
+
 function Voctopus(depth) {
+	/**
+	 * @property _elements
+	 * @private 
+	 * Internal array storing the individual data for each octet. We use a
+	 * standard JS Array here because they're sparse by nature, so having empty
+	 * octets has a very small memory footprint even for very large sets.
+	 */
 	var _elements = [];
-	var _depth = depth; 
-	function octCoord(coord, d) {
-		return (((coord[2] >>> _depth-d) & 1) << 2 | 
-						((coord[1] >>> _depth-d) & 1) << 1 |
-						((coord[0] >>> _depth-d) & 1) 
-					 );
+	/**
+	 * @property _cleanupQueue
+	 * @private 
+	 *
+	 * A queue of octets that need cleanup after a mutating operation. This
+	 * may consist of removing or regenerating suboctets depending on whether
+	 * the child octets are uniform.
+	 */
+	var _updateQueue = [];
+
+	/**
+	 * @property _md
+	 * @private
+	 * The maximum depth of the tree, in other words the depth at which a single
+	 * node is 1 unit wide. Traversal stops at this depth. Also determines the
+	 * maximum size of a fully dense Voctopus.
+	 */
+	var _md = depth; 
+	/*
+	this.updateTree = function() {
+		console.log("Updating octree with queue length "+_updateQueue.length);
+		var time = new Date().getTime();
+		voctopusUpdateTree.call(this, _elements, _updateQueue, _md);
+		console.log("Finished tree update, new queue length: "+_updateQueue.length);
+		console.log("Update took "+(new Date().getTime() - time)+" milliseconds");
 	}
-}
+	*/
+	this.updateTree = voctopusUpdateTree.bind(this, _elements, _updateQueue, _md);
 
-Voctopus.prototype.octAt = function(coord) {
-	var depth, index;
-	if(arguments.length === 2) depth = arguments[1];
-	else depth = 0;
-}
+	/**
+	 * The following methods are implemented inside Voctopus for access to private
+	 * properties. This is faster than setting them on the prototype.
+	 */
 
-Voctopus.prototype.octId = function(coord, d) {
-	var i;
-	var o = 0;
-	for(i = _depth-d; i < _depth; i++) {
-		o += octCoord(coord, i) + baseOffset(d);	
-	}
-}
+	/**
+	 * Returns the identity (0-7) of the suboctet at depth d, coord.
+	 * @param v {vector} global coordinate of voxel
+	 * @param d {int} depth to check
+	 *
+	 */
+	this.octId = voctopusOctId.bind(this, _md);
 
+	/**
+	 * Finds the key of an octet given coordinates and depth.
+	 * @param v {vector} cordinate vector of the voxel at d = maxDepth
+	 * @param d {int} tree depth (defaults to maxDepth)
+	 */
+	this.octKey = voctopusOctKey.bind(this, _md);
+
+	/**
+	 * Traverses the tree in a depth-first search until it reaches an empty
+	 * octet at d+1, indicating that octet is uniform with the octet value at d
+	 * @param v {vector} voxel coordinate
+	 */
+	this.getVoxel = voctopusGetVoxel.bind(this, _elements);
+
+	/**
+	 * Sets the value of a voxel, then flags the node for pruning. If you're modifying
+	 * a bunch of voxels in one operation, use Voctopus#setVoxels instead, so that
+	 * pruning is deferred until after completing the updates.
+	 */
+	this.setVoxel = voctopusSetVoxel.bind(this, _updateQueue, _elements, _md, true);
+
+	this.setVoxels = voctopusSetVoxels.bind(this, _updateQueue, _elements, _md);
+
+	this.depthOf = voctopusDepthOf.bind(this, _md);
+	this.vecOf = voctopusVecOf.bind(this);
+	this.parentOfKey = voctopusParentOfKey.bind(this, _md);
+
+	/**
+	 * Expose the _elements array for testing. 
+	 * DO NOT USE THIS IN PRODUCTION IT'S DANGEROUS AND POINTLESS SINCE YOU
+	 * CAN'T MAKE CHANGES
+	 */
+	Object.defineProperty(this, "_elements", { 
+		get:function() {return _elements.slice(0)},
+		enumerable:false
+	});
+}
 
 if(typeof(module) !== "undefined") module.exports = Voctopus;
--- test/test-voctopus.js
+++ test/test-voctopus.js
@@ -3,22 +3,184 @@ require("should");
 var Voctopus = require("../src/voctopus");
 
 describe("voctopus", function() {
-	var voc = new Voctopus(5);
-	it("should yield expected octet ids", function () {
-		// always 0 at depth 0
-		voc.octId([5,5,5], 0).should.equal(0);
-		voc.octId([5,5,5], 0).should.equal(0);
-		voc.octId([5,5,5], 0).should.equal(0);
-		voc.octId([5,5,5], 0).should.equal(0);
+	var d = 5;
+	var voc = new Voctopus(d);
+	function isUndef(val) {
+		return (typeof(val) === "undefined");
+	}
+	it("should always return 0 at depth 0 for octId", function() {
+		voc.octId([ 0, 0, 0], 0).should.equal(0);
+		voc.octId([31, 0, 0], 0).should.equal(0);
+		voc.octId([ 0,31, 0], 0).should.equal(0);
+		voc.octId([31,31, 0], 0).should.equal(0);
+		voc.octId([ 0, 0,31], 0).should.equal(0);
+		voc.octId([31, 0,31], 0).should.equal(0);
+		voc.octId([ 0,31,31], 0).should.equal(0);
+		voc.octId([31,31,31], 0).should.equal(0);
+	});
+	it("should yield expected octet identities (range 0-7) for a coord", function() {
+		var i;
+		// These should have the same identity at any depth
+		for(i = 1; i < d; i++) {
+			voc.octId([ 0, 0, 0], i).should.equal(0);
+			voc.octId([31, 0, 0], i).should.equal(1);
+			voc.octId([ 0,31, 0], i).should.equal(2);
+			voc.octId([31,31, 0], i).should.equal(3);
+			voc.octId([ 0, 0,31], i).should.equal(4);
+			voc.octId([31, 0,31], i).should.equal(5);
+			voc.octId([ 0,31,31], i).should.equal(6);
+			voc.octId([31,31,31], i).should.equal(7);
+		}
+		// for d == 2, coordinates corresponding to octet 8 at d == 1)
+		for(i = 2; i < d; i++) {
+			voc.octId([ 0, 0, 0], 2).should.equal(0);
+			voc.octId([15, 0, 0], 2).should.equal(1);
+			voc.octId([ 0,15, 0], 2).should.equal(2);
+			voc.octId([15,15, 0], 2).should.equal(3);
+			voc.octId([ 0, 0,15], 2).should.equal(4);
+			voc.octId([15, 0,15], 2).should.equal(5);
+			voc.octId([ 0,15,15], 2).should.equal(6);
+			voc.octId([15,15,15], 2).should.equal(7);
+		}
+	});
+	it("should yield expected octet keys with octKey", function () {
+		// always 0 at depth 0 - everything is within the root (id == 0)
+	 	voc.octKey([5,5,5], 0).should.equal(0);
+		voc.octKey([5,5,5], 0).should.equal(0);
+		voc.octKey([5,5,5], 0).should.equal(0);
+		voc.octKey([5,5,5], 0).should.equal(0);
 
-		// at depth 1, should get 1-8
-		voc.octId([ 0, 0, 0], 0).should.equal(1);
-		voc.octId([16, 0, 0], 0).should.equal(2);
-		voc.octId([ 0,16, 0], 0).should.equal(3);
-		voc.octId([16,16, 0], 0).should.equal(4);
-		voc.octId([ 0, 0,16], 0).should.equal(5);
-		voc.octId([16, 0,16], 0).should.equal(6);
-		voc.octId([ 0,16,16], 0).should.equal(7);
-		voc.octId([16,16,16], 0).should.equal(8);
+		// at depth 1, should get 1-8 for the following coordinates
+		// corresponding to the first offset:
+		voc.octKey([ 0, 0, 0], 1).should.equal(1);
+		voc.octKey([31, 0, 0], 1).should.equal(2);
+		voc.octKey([ 0,31, 0], 1).should.equal(3);
+		voc.octKey([31,31, 0], 1).should.equal(4);
+		voc.octKey([ 0, 0,31], 1).should.equal(5);
+		voc.octKey([31, 0,31], 1).should.equal(6);
+		voc.octKey([ 0,31,31], 1).should.equal(7);
+		voc.octKey([31,31,31], 1).should.equal(8);
+
+		// at depth 2, should get for the following coordinates
+		voc.octKey([ 0, 0, 0], 2).should.equal(9);
+		voc.octKey([ 8, 0, 0], 2).should.equal(10);
+		voc.octKey([ 0, 8, 0], 2).should.equal(11);
+		voc.octKey([ 8, 8, 0], 2).should.equal(12);
+		voc.octKey([ 0, 0, 8], 2).should.equal(13);
+		voc.octKey([ 8, 0, 8], 2).should.equal(14);
+		voc.octKey([ 0, 8, 8], 2).should.equal(15);
+		voc.octKey([ 8, 8, 8], 2).should.equal(16);
+		voc.octKey([16, 0, 0], 2).should.equal(17);
+		voc.octKey([ 0,16, 0], 2).should.equal(25);
+		voc.octKey([16,16, 0], 2).should.equal(33);
+		voc.octKey([ 0, 0,16], 2).should.equal(41);
+		voc.octKey([16, 0,16], 2).should.equal(49);
+		voc.octKey([ 0,16,16], 2).should.equal(57);
+		voc.octKey([16,16,16], 2).should.equal(65);
+
+		// At depth 3, we should get:
+		voc.octKey([ 0, 0, 0], 3).should.equal(73);
+		voc.octKey([ 4, 0, 0], 3).should.equal(74);
+		voc.octKey([ 0, 4, 0], 3).should.equal(75);
+		voc.octKey([ 8, 0, 0], 3).should.equal(137);
+		voc.octKey([ 8, 4, 0], 3).should.equal(139);
+		voc.octKey([ 0, 8, 0], 3).should.equal(201);
+		voc.octKey([ 4, 8, 0], 3).should.equal(202);
+
+		// At depth 4 we should get:
+		voc.octKey([ 0, 0, 0], 4).should.equal(585);
+		voc.octKey([ 2, 0, 0], 4).should.equal(586);
+
+		// At depth 5 we should get:
+		voc.octKey([ 0, 0, 0], 5).should.equal(4681);
+		voc.octKey([ 1, 0, 0], 5).should.equal(4682);
+		
+		// pretty sure if all those are coming up correct we probably have the right
+		// algorithm! It gets to deep to reason out on paper beyond this
+	});
+	it("should find the tree depth of a key with depthOf", function() {
+		// first offsets of each depth
+		voc.depthOf(0).should.equal(0);
+		voc.depthOf(1).should.equal(1);
+		voc.depthOf(9).should.equal(2);
+		voc.depthOf(73).should.equal(3);
+		voc.depthOf(585).should.equal(4);
+		voc.depthOf(4681).should.equal(5);
+		// sample other keys
+		voc.depthOf(6).should.equal(1);
+		voc.depthOf(10).should.equal(2);
+		voc.depthOf(282).should.equal(3);
+		voc.depthOf(584).should.equal(3);
+		voc.depthOf(7000).should.equal(5);
+	});
+	it("should find a vector for an octet identity with vecOf", function() {
+		voc.vecOf(0).should.eql([0,0,0]);
+		voc.vecOf(1).should.eql([1,0,0]);
+		voc.vecOf(2).should.eql([0,1,0]);
+		voc.vecOf(3).should.eql([1,1,0]);
+		voc.vecOf(4).should.eql([0,0,1]);
+		voc.vecOf(5).should.eql([1,0,1]);
+		voc.vecOf(6).should.eql([0,1,1]);
+		voc.vecOf(7).should.eql([1,1,1]);
+	});
+	it("should find the parent octet key of an octet key with parentOfKey", function() {
+		voc.parentOfKey(1).should.equal(0);
+		voc.parentOfKey(3).should.equal(0);
+		voc.parentOfKey(8).should.equal(0);
+		voc.parentOfKey(9).should.equal(1);
+		voc.parentOfKey(13).should.equal(1);
+		voc.parentOfKey(17).should.equal(2);
+		voc.parentOfKey(48).should.equal(5);
+		voc.parentOfKey(585).should.equal(73);
+		voc.parentOfKey(593).should.equal(74);
+		voc.parentOfKey(4681).should.equal(585);
+	});
+	it("should set a voxel at the right key with setVoxel", function() {
+		var voc = new Voctopus(2); // let's make a 8x8x8 tree
+		voc.setVoxel([0,0,0], 1);
+		voc._elements[9].should.equal(1);
+		// voctopus trees are basic Arrays, so they should be heterogeneous
+		voc.setVoxel([1,0,0], "hello");
+		voc._elements[10].should.equal("hello");
+		voc.setVoxel([2,0,0], "goodbye");
+		voc._elements[17].should.equal("goodbye");
+		voc.setVoxel([7,7,7], 42);
+		voc._elements[72].should.equal(42);
+	});
+	it("should set multiple voxels at once using setVoxels", function() {
+		var voc = new Voctopus(2); // let's make a 8x8x8 tree
+		voc.setVoxels([
+			[[0,0,0], 1],
+			[[1,0,0], "hello"],
+			[[2,0,0], "goodbye"],
+			[[7,7,7], 42]
+		]);
+		voc._elements[9].should.equal(1);
+		voc._elements[10].should.equal("hello");
+		voc._elements[17].should.equal("goodbye");
+		voc._elements[72].should.equal(42);
+	});
+	it("should update the tree after setting a voxel", function() {
+		var i;
+		var voc = new Voctopus(2); // let's make a 8x8x8 tree
+		voc.setVoxels([
+			[[0,0,0], 1],
+			[[1,0,0], 1],
+			[[0,1,0], 1],
+			[[1,1,0], 1],
+			[[0,0,1], 1],
+			[[1,0,1], 1],
+			[[0,1,1], 1],
+			[[1,1,1], 1]
+		]);
+		// should have pruned the tree
+		for(i = 0; i < 8; i++) isUndef(voc._elements[9+i]).should.be.true();
+
+		// parents should be set
+		isUndef(voc._elements[1]).should.be.false();
+		voc._elements[1].should.equal(1);
+
+		// check a bigger volume just for shits and giggles
+		voc = new Voctopus(3);
 	});
 });
